65eeb71f52f3273d099b38c7f400fa23c0c432f1
leeyunbo <servers1@naver.com>
2020-04-13 13:11:56 +0900

    MainView <-> MainViewModel <-> MainModel DataBinding 완료

--- app/src/main/java/com/leeyunbo/coronaboard/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/leeyunbo/coronaboard/viewmodel/MainViewModel.kt
@@ -1,13 +1,15 @@
 package com.leeyunbo.coronaboard.viewmodel
 
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.leeyunbo.coronaboard.data.CoronaBoard
 import com.leeyunbo.coronaboard.data.MainModel
 import com.leeyunbo.coronaboard.data.Model
+import java.util.*
 
 class MainViewModel : ViewModel {
-    val coronaBoard = MutableLiveData<CoronaBoard>()
+    val coronaBoard = ObservableField<CoronaBoard>()
     override val model: Model = MainModel()
 
     override fun doAction() {
@@ -15,7 +17,7 @@ class MainViewModel : ViewModel {
     }
 
     fun completeGetData(_coronaBoard: CoronaBoard) {
-        coronaBoard.postValue(_coronaBoard)
-        System.out.println(coronaBoard.value.toString())
+        coronaBoard.set(_coronaBoard)
+        System.out.println(coronaBoard.get().toString())
     }
 }
